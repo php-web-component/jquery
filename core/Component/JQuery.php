eeb15d8373b9ac53d07ddd790e8105b0aa6cd7c0
Bagus A Priambada <iam.adty@gmail.com>
2020-11-17 18:38:20 +0700

    use pwc static config

--- core/Component/JQuery.php
+++ core/Component/JQuery.php
@@ -5,7 +5,7 @@ class JQuery extends \PWC\Component
     public function render(): string
     {
         return (string) \PWC\Component\Html\Script::build([
-            \PWC\AssetsManager\Config::instance()->get('dir') . 'php-web-component/jquery/jquery.min.js'
+            \PWC\AssetsManager\Config::get('dir') . 'php-web-component/jquery/jquery.min.js'
         ]);
     }
 
