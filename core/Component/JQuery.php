1034fd162cd9aa6100dc44e2cc5aac195ac447c1
Bagus A Priambada <iam.adty@gmail.com>
2020-11-25 13:40:49 +0700

    jquery with decorator

--- core/Component/JQuery.php
+++ core/Component/JQuery.php
@@ -1,33 +1,37 @@
 <?php namespace PWC\Component;
 
-use PWC\AssetsManager\Config;
 use PWC\BuilderTrait;
 use PWC\Component;
 use PWC\Component\Html\Body;
 use PWC\Component\Html\Head;
 use PWC\Component\Html\Script;
 use PWC\Component\Html\Title;
+use PWC\Config\Asset;
 
 class JQuery extends Component
 {
     protected $_ID = 'pwc-jquery';
 
-    protected $html = [];
-    protected $head = [];
-    protected $body = [];
-    protected $title = 'JQuery';
+    protected ?Html $html;
+    protected ?Head $head;
+    protected ?Body $body;
+    protected ?Title $title;
 
     public function render(): string
     {
-        return (string) Html::build(
-            Head::build(
-                Title::build(Text::build($this->title)),
-            )->decorate($this->head),
-            Body::build(
+        return (string) ($this->html ?? Html::build(
+            ($this->head ?? Head::build(
+                ($this->title ?? Title::build(
+                    Text::build('JQuery'),
+                ))->withDecorators($this->_decorators),
+            ))->withDecorators($this->_decorators),
+            ($this->body ?? Body::build(
                 parent::render(),
-                Script::register(Config::get('dir') . 'php-web-component/jquery/jquery.min.js'),
-            )->decorate($this->body),
-        )->decorate($this->html);
+                Script::register(Asset::get('dir') . 'php-web-component/jquery/jquery.min.js'),
+
+                ...$this->_decorators->get(),
+            ))->withDecorators($this->_decorators),
+        ))->withDecorators($this->_decorators);
     }
 
     use BuilderTrait;
